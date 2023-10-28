e85644aadf27d4aba8d0fba02c1a987c72554f9f
Arnab <bhowmik.arnab15@gmail.com>
2023-10-28 16:21:33 +1100

    Updating SignUp process to add new User to MongoDB if doesn't exist already. Also clearing old code

--- auth-service/src/routes/signup.ts
+++ auth-service/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-errors';
-import { DatabaseConnectionError } from '../errors/database-connection-errors';
 
 const router = express.Router();
 
@@ -14,17 +14,28 @@ router.post('/api/users/signup', [
          .isLength({ min: 4, max: 20 })
          .withMessage('Password length must be between 4 and 20 characters')
     ], 
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        // Check for any Validation errors
         const errors = validationResult(req);
-
         if (!errors.isEmpty()) {
             throw new RequestValidationError(errors.array());
         }
 
         console.log('Signing Up new User...');
-        throw new DatabaseConnectionError();
 
-        res.send({});
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        
+        //Check if any User already exists with the same email
+        if (existingUser) {
+            console.log('Provided Email already exists!');
+            return res.send({});
+        } else {
+            // Creat the User & save in corresponding MongoDB Collection
+            const user = User.build({ email, password });
+            await user.save();
+            res.status(201).send(user);
+        }
 });
 
 export { router as signUpRouter };
