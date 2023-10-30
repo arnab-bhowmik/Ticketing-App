5fdf25ada57cb15836e76e27cefe6a9cff8b1267
Arnab <bhowmik.arnab15@gmail.com>
2023-10-30 16:44:32 +1100

    Updated SignIn route with validation check & error

--- auth-service/src/routes/signin.ts
+++ auth-service/src/routes/signin.ts
@@ -1,8 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-errors';
+
 const router = express.Router();
 
-router.get('/api/users/signin', (req, res) => {
-    res.send('Successfully Signed In!');
-});
+router.post('/api/users/signin', [
+    body('email')
+     .isEmail()
+     .withMessage('Email must be valid'),
+     body('password')
+     .trim()
+     .notEmpty()
+     .withMessage('Password is mandatory!')
+    ], (req: Request, res: Response) => {
+        // Check for any Validation errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            throw new RequestValidationError(errors.array());
+        }
+    }
+);
 
 export { router as signInRouter };
