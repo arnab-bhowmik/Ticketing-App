5391492b5c8aa91cc88468261adc16e52b3c33a3
Arnab <bhowmik.arnab15@gmail.com>
2023-10-30 18:14:25 +1100

    Updates to check JWT token validity for LoggedUser

--- auth-service/src/routes/currentuser.ts
+++ auth-service/src/routes/currentuser.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-    res.send('Hi There Visitor!');
+    // Check if JWT exists in the Cookie session
+    if (!req.session?.jwt) {
+        console.log('JWT does not exist or has expired!');
+        return res.send({ currentUser: null });
+    } else {
+        try{
+            const jwtPayload = jwt.verify(req.session?.jwt, process.env.JWT_KEY!);
+            res.send({ currentUser: jwtPayload });
+        } catch (err) {
+            console.log('Error encountered while verifying the JWT!', err);
+            res.send({ currentUser: null });
+        }
+    }
 });
 
 export { router as currentUserRouter };
