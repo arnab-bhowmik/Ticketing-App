92c76b5f53675560fca99fc79ae1055e5449e2fd
Arnab <bhowmik.arnab15@gmail.com>
2023-11-02 14:58:02 +1100

    Using a common hook for handling UI errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,22 @@
-import { useState } from "react";
 import axios from 'axios';
+import { useState } from "react";
+import useRequest from '../../hooks/use-request';
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors } = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        }
+    });
 
     const onSubmit = async (event) => {
         event.preventDefault();
         // Define what happens once User attempts to Sign Up!
-        try {
-            const response = await axios.post('/api/users/signup', {
-                email, password
-            });
-            console.log(response.data); 
-        } catch (error) {
-            setErrors(error.response.data.errors);
-        }
+        doRequest();
     };
 
     return <form onSubmit={onSubmit}>
@@ -29,13 +29,7 @@ export default () => {
             <label>Password</label>
             <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control"/>
         </div>
-        {errors.length > 0 && 
-        <div className="alert alert-danger">
-            <h4>Oops...</h4>
-            <ul className="my-0">
-                {errors.map(err => <li key={err.message}>{err.message}</li>)}
-            </ul>
-        </div>}
+        {errors}
         <button className="btn btn-primary">Sign Up</button>
     </form>;
 };
