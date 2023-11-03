2696f7e3c85444d60b5a4994e43059e57db004ef
Arnab <bhowmik.arnab15@gmail.com>
2023-11-03 22:28:36 +1100

    Use getInitialProps method to call Ingress for SSR

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,24 @@
-const Landing = () => {
+import axios from 'axios';
+
+const Landing = ({ currentUser }) => {
+    console.log(currentUser);
     return <h1>Landing Page</h1>;
+};
+
+// Method allows to fetch data that needs to be shown on the UI Component
+Landing.getInitialProps = async ({ req }) => {
+    console.log(req.headers);
+    if (typeof window === 'undefined') {
+        // We are on the Server, hence requests should be made to http://{Ingress_Service_Name}.{Ingress_Service_NameSpace}.svc.cluster.local/{Route_Path}
+        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+            headers: req.headers
+        }); 
+        return data;
+    } else {
+        // We are on the Browser, hence requests need not have a base url of some kind rather should only specify the path of the route
+        const { data } = await axios.get('/api/users/currentuser'); 
+        return data;
+    }
 };
    
 export default Landing;
