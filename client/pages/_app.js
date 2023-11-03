c8ea572164d83c4383bfcd9c4d9e4c06b3768dd7
Arnab <bhowmik.arnab15@gmail.com>
2023-11-03 23:59:42 +1100

    Add getInitialProps method on custom App Component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
+const AppComponent = ({ Component, pageProps }) => {
     return <Component {...pageProps} />
 };
+
+AppComponent.getInitialProps = async (context) => {
+    const { data } = await buildClient(context.ctx).get('/api/users/currentuser');
+    
+    // Following is the calling of the method getInitialProps for Landing Page so that it can be passed to index.js i.e. Landing Page!
+    let landingPageProps = {};
+    if (context.Component.getInitialProps) {
+        landingPageProps = await context.Component.getInitialProps(context.ctx);
+    }
+    console.log(landingPageProps);
+    return data;
+};
+
+export default AppComponent;
