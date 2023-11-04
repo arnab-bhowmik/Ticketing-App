dd1083cca3d3b79384194e8c171f7133a88589af
Arnab <bhowmik.arnab15@gmail.com>
2023-11-04 19:12:35 +1100

    Return o/p from getInitialProps method on UI

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,20 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from "../api/build-client";
 
-const AppComponent = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+    <div>
+        <h1>Email of the User - {currentUser.email}</h1>
+        <Component {...pageProps} />
+    </div>
+    );
 };
 
 AppComponent.getInitialProps = async (context) => {
     const { data } = await buildClient(context.ctx).get('/api/users/currentuser');
     
     // Following is the calling of the method getInitialProps for Landing Page so that it can be passed to index.js i.e. Landing Page!
-    let landingPageProps = {};
+    let pageProps = {};
     if (context.Component.getInitialProps) {
-        landingPageProps = await context.Component.getInitialProps(context.ctx);
+        pageProps = await context.Component.getInitialProps(context.ctx);
     }
-    console.log(landingPageProps);
-    return data;
+
+    return { pageProps, ...data };
 };
 
 export default AppComponent;
