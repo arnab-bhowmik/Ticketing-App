d7b506ac707db3c7c093e00c452bb70e38155787
Arnab <bhowmik.arnab15@gmail.com>
2023-11-04 20:01:34 +1100

    Introducing Sign Out Feature

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import Router from 'next/router';
+import { useEffect } from "react";
+import useRequest from '../../hooks/use-request';
+
+export default () => {
+    const { doRequest } = useRequest({
+        url: '/api/users/signout',
+        method: 'post',
+        body: {},
+        onSuccess: () => Router.push('/')
+    });
+
+    useEffect(() => {
+        doRequest();
+    }, []);
+
+    return <div>Signing you out...!</div>;
+};
