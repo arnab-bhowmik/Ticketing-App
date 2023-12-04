4e007e131a432f428c22bd00f32bcf6883e6ad30
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2023-12-04 14:19:31 +1100

    Introducing the Order MicroService

---
+++ order-service/src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import 'express-async-errors'; 
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+import { errorHandler, NotFoundError, currentUser } from "@ticketing_org/custom-modules";
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false
+    // secure: true
+}));
+app.use(currentUser);
+
+app.get('*', async () => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
