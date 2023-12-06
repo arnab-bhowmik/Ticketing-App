b00a422ed155736f83083ad60381ff4f4b36bb93
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2023-12-06 13:08:13 +1100

    Added Test Scenarios for Cancel Order route

---
+++ order-service/src/routes/__test__/deleteorder.test.ts
@@ -0,0 +1,41 @@
+import request from "supertest";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order, OrderStatus } from "../../models/order";
+
+// ------------ Test Scenarios for updating status for a specific Order to Cancelled ------------
+
+it('returns an error if a different user tries to fetch another user order for marking it deleted', async () => {
+    // Create a Concert Ticket 
+    const ticket = Ticket.build({ title: 'concert', price: 200 });
+    await ticket.save();
+
+    // Create Two Users
+    const user1 = global.signin();
+    const user2 = global.signin();
+
+    // Create a new Order as user1 and associate ticket with this Order
+    const { body: order } = await request(app).post('/api/orders').set('Cookie', user1).send({ ticketId: ticket.id }).expect(201);
+
+    // Send a request to mark the Order as cancelled as user2
+    await request(app).delete(`/api/orders/${order.id}`).set('Cookie', user2).expect(401);
+});
+
+it('marks a particular Order as Cancelled', async () => {
+    // Create a Concert Ticket 
+    const ticket = Ticket.build({ title: 'concert', price: 200 });
+    await ticket.save();
+
+    // Create a User
+    const user = global.signin();
+
+    // Create a new Order as user and associate ticket with this Order
+    const { body: order } = await request(app).post('/api/orders').set('Cookie', user).send({ ticketId: ticket.id }).expect(201);
+
+    // Send a request to mark the Order as cancelled as the same user
+    await request(app).delete(`/api/orders/${order.id}`).set('Cookie', user).expect(204);
+
+    // Send a request to fetch details of the Order as the same user and confirm the status has been updated
+    const { body: updatedOrder } = await request(app).get(`/api/orders/${order.id}`).set('Cookie', user);
+    expect(updatedOrder.status).toEqual(OrderStatus.Cancelled);
+});
--- order-service/src/routes/deleteorder.ts
+++ order-service/src/routes/deleteorder.ts
@@ -1,9 +1,22 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest, NotFoundError, BadRequestError, NotAuthorizedError } from '@ticketing_org/custom-modules';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-    res.status(200).send({});
+router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+    // Retrieve the Order details for the provided Order Id
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+    if (!order) {
+        throw new NotFoundError();
+    } 
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthorizedError();
+    }
+    // Update the Order Status to Cancelled
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+    res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
