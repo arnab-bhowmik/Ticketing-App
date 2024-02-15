d1a209ebd9c9a2e5cf35d204c1d2d6bed48cfe3d
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-02-15 11:45:05 +0530

    Following changes, 1) Add logic in Ticket MicroService to handle incoming Order Create/Cancelled Events 2) Once Ticket record is updated (in Ticket MicroService) wrt Order Create/Cancelled Events, also emit Ticket Updated event

--- ticket-service/src/events/listeners/order-cancelled-listener.ts
+++ ticket-service/src/events/listeners/order-cancelled-listener.ts
@@ -1,5 +1,8 @@
 import amqp from 'amqplib';
 import { Subjects, Listener, OrderCancelledEvent } from "@ticketing_org/custom-modules";
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
+import { connection, exchange } from '../../index';
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
     subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
@@ -8,7 +11,23 @@ export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
         // Check the 'type' property of the incoming event message and validate if it matches the Event Subject for this listener class 
         const eventType = msg.properties.type.toString();
         if (eventType === Subjects.OrderCancelled) {
-            // To-Do :- Set the incoming event attribute values to unreserve a ticket record
+            // Find the ticket that the Order is reserving
+            const ticket = await Ticket.findById(data.ticket.id);
+            if (!ticket) {
+                throw new Error('Ticket not found');
+            }
+            // Mark the ticket as being unreserved by setting its orderId property to undefined
+            ticket.set({ orderId: undefined });
+            await ticket.save();
+            // Publish an event for Ticket Update
+            await new TicketUpdatedPublisher(connection!,exchange).publish({
+                id:      ticket.id,
+                version: ticket.version,
+                title:   ticket.title,
+                price:   ticket.price,
+                userId:  ticket.userId,
+                orderId: ticket.orderId
+            });
             return Boolean(true);
         } else {
             return Boolean(false);
--- ticket-service/src/events/listeners/order-created-listener.ts
+++ ticket-service/src/events/listeners/order-created-listener.ts
@@ -1,5 +1,8 @@
 import amqp from 'amqplib';
 import { Subjects, Listener, OrderCreatedEvent } from "@ticketing_org/custom-modules";
+import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
+import { connection, exchange } from '../../index';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -8,7 +11,23 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
         // Check the 'type' property of the incoming event message and validate if it matches the Event Subject for this listener class 
         const eventType = msg.properties.type.toString();
         if (eventType === Subjects.OrderCreated) {
-            // To-Do :- Set the incoming event attribute values to reserve a ticket record 
+            // Find the ticket that the Order is reserving
+            const ticket = await Ticket.findById(data.ticket.id);
+            if (!ticket) {
+                throw new Error('Ticket not found');
+            }
+            // Mark the ticket as being reserved by setting its orderId property
+            ticket.set({ orderId: data.id });
+            await ticket.save();
+            // Publish an event for Ticket Update
+            await new TicketUpdatedPublisher(connection!,exchange).publish({
+                id:      ticket.id,
+                version: ticket.version,
+                title:   ticket.title,
+                price:   ticket.price,
+                userId:  ticket.userId,
+                orderId: ticket.orderId
+            });
             return Boolean(true);
         } else {
             return Boolean(false);
