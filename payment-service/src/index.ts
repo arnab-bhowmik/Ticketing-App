7bb5ec2729287640f9ace24da54035f1efb6d7c8
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-02-17 09:19:22 +0530

    Adding the Payment Service codebase

---
+++ payment-service/src/index.ts
@@ -0,0 +1,64 @@
+import amqp from 'amqplib';
+import mongoose from "mongoose";
+import { app } from "./app";
+import { openRabbitMQConnection } from "@ticketing_org/custom-modules";
+// import { OrderCreatedListener } from './events/listeners/order-created-listener';
+// import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
+
+let rabbitmqUsername: string, rabbitmqPassword: string, rabbitmqService: string;
+let connection: amqp.Connection;
+let exchange: string, queue: string;
+
+const startUp = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined!');
+    }
+
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined!');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Successfully connected to Mongo DB');
+    } catch (err) {
+        console.log(err);
+    }
+
+    if (!process.env.RABBITMQ_USERNAME) {
+        throw new Error('RABBITMQ_USERNAME must be defined!');
+    }
+    if (!process.env.RABBITMQ_PASSWORD) {
+        throw new Error('RABBITMQ_PASSWORD must be defined!');
+    }
+    if (!process.env.RABBITMQ_SERVICE) {
+        throw new Error('RABBITMQ_SERVICE must be defined!');
+    }
+    if (!process.env.RABBITMQ_EXCHANGE) {
+        throw new Error('RABBITMQ_EXCHANGE must be defined!');
+    }
+    if (!process.env.RABBITMQ_QUEUE) {
+        throw new Error('RABBITMQ_QUEUE must be defined!');
+    }
+
+    rabbitmqUsername = process.env.RABBITMQ_USERNAME;
+    rabbitmqPassword = process.env.RABBITMQ_PASSWORD;
+    rabbitmqService  = process.env.RABBITMQ_SERVICE;
+    exchange         = process.env.RABBITMQ_EXCHANGE;
+    queue            = process.env.RABBITMQ_QUEUE;
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!!');
+    });
+
+    // Establish connection with RabbitMQ service for consuming Events. Keep the connection open.
+    connection = (await openRabbitMQConnection(rabbitmqUsername,rabbitmqPassword,rabbitmqService))!;
+    
+    // // Listen for Order Creation events
+    // await new OrderCreatedListener(connection!, queue).listen();
+    // // Listen for Order Cancellation events
+    // await new OrderCancelledListener(connection!, queue).listen();
+}
+
+startUp();
+export { connection, exchange, queue };
---
+++ payment-service/src/test/setup.ts
@@ -0,0 +1,66 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import request from "supertest";
+import { app } from '../app';
+import jwt from 'jsonwebtoken';
+
+declare global {
+    namespace NodeJS {
+        interface Global {
+            signin() : string[];
+        } 
+    }
+}
+
+let mongo: any;
+process.env.JWT_KEY = 'fgfhjhukhbdxhgjhk';
+
+// Execute the function at the beginning of a Test Cycle
+beforeAll(async () => {
+    mongo = await MongoMemoryServer.create();
+    const mongoUri = mongo.getUri();
+
+    await mongoose.connect(mongoUri, {})
+});
+
+// Execute the function before each Test within a Test Cycle
+beforeEach(async () => {
+    const collections = await mongoose.connection.db.collections();
+
+    for (let collection of collections) {
+        await collection.deleteMany({});
+    }
+});
+
+// Execute the function at the end of a Test Cycle
+afterAll(async () => {
+    if (mongo) {
+      await mongo.stop();
+    }
+    await mongoose.connection.close();
+});
+
+// Have a cookie created on User Sign Up to be used by other test scripts 
+global.signin = () => {
+
+    // Build a JWT Payload - { id, email }
+    const payload = {
+        id: new mongoose.Types.ObjectId().toHexString(),
+        email: 'user1@abc.com'
+    };
+
+    // Create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+    // Build Session Object { jwt: JWT_KEY }
+    const session = { jwt: token };
+
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
+
+    // Take session JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    // Return string containing the base64 encoded cookie
+    return [`session=${base64}`];
+};
