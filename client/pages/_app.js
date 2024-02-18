b1c5d2f6ba8281f71e7fa355227208812f4053ca
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-02-18 16:30:29 +0530

    App Component will pass Current User & Build Client details to child components via getInitialProps() method

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,24 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import buildClient from "../api/build-client";
-import header from '../components/header';
 import Header from '../components/header';
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
     return (
-    <div>
-        <Header currentUser={currentUser} />
-        <Component {...pageProps} />
-    </div>
+        <div>
+            <Header currentUser={currentUser} />
+            <Component currentUser={currentUser} {...pageProps} />
+        </div>
     );
 };
 
 AppComponent.getInitialProps = async (context) => {
-    const { data } = await buildClient(context.ctx).get('/api/users/currentuser');
+    const client = buildClient(context.ctx);
+    const { data } = await client.get('/api/users/currentuser');
     
     // Following is the calling of the method getInitialProps for Landing Page so that it can be passed to index.js i.e. Landing Page!
     let pageProps = {};
     if (context.Component.getInitialProps) {
-        pageProps = await context.Component.getInitialProps(context.ctx);
+        pageProps = await context.Component.getInitialProps(context.ctx, client, data.currentUser);
     }
 
     return { pageProps, ...data };
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,14 @@
-import buildClient from "../api/build-client";
+// import buildClient from "../api/build-client";
 
 const Landing = ({ currentUser }) => {
     return currentUser ? <h1>Signed In Successfully!</h1> : <h1>User not Signed In!</h1>
 };
 
 // Method allows to fetch data that needs to be shown on the UI Component. Importing buildClient lets us create an Axios instance.
-Landing.getInitialProps = async (context) => {
-    const { data } = await buildClient(context).get('/api/users/currentuser');
-    return data;
+Landing.getInitialProps = async (context, client, currentUser) => {
+    // const { data } = await buildClient(context).get('/api/users/currentuser');
+    // return data;
+    return {};
 };
    
 export default Landing;
