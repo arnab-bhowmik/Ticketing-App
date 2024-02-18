73c3cba591ea89fc87c6ab4c522ab3a4a123caf5
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-02-18 22:00:21 +0530

    Added Stripe checkout for Ticket's Order Payment

--- client/pages/order/[orderId].js
+++ client/pages/order/[orderId].js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from 'react-stripe-checkout';
+import Router from 'next/router';
+import useRequest from "../../hooks/use-request";
 
-const showOrder = ({ order }) => {
+const showOrder = ({ order, currentUser }) => {
     const [timeLeft, setTimeLeft] = useState(0);
+
+    // doRequest function invokes the useRequest() method which in turn calls the backend route
+    const { doRequest, errors } = useRequest({
+        url: '/api/payments',
+        method: 'post',
+        body: { orderId: order.id  },                   // The body also contains the token param which is incorporated by doRequest() function under the hood
+        onSuccess: (payment) => console.log(payment)
+    });
 
     useEffect(() => {
         // Function findTimeLeft() when invoked will update the time remaining until Order expiry via setTimeLeft()
@@ -21,13 +32,21 @@ const showOrder = ({ order }) => {
     }, []);
 
     if (timeLeft < 0) {
-        return <div>Order Expired</div>
+        return <div><h2>Order Expired</h2></div>
     }
     
     return (
         <div>
           <h1>Order Id: {order.id}</h1>
           <h4>Time left to pay: {timeLeft} seconds </h4>
+          <StripeCheckout 
+            // The doRequest() function is being invoked using the token as an additional property so that it will be used within the body while making a request to the backend route
+            token={({ id }) => doRequest({ token: id })}
+            stripeKey="pk_test_51OklBxSGpgG0gu7SuyWdBvpPt7eAapz48l7q5IE5Tu52kYhs6Fd8zMwTOZAKnse3yFUYd1TyCvylc71wcClww83B00dBLiGYYY"
+            amount={order.ticket.price * 100}
+            email={currentUser.email}
+          />
+          {errors}
         </div>
     );
 };
