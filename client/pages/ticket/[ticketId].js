c5086daffb21689ea9978aa8808e27259b2b3c56
Arnab Bhowmik <bhowmik.arnab15@gmail.com>
2024-02-26 21:54:04 +0530

    Minor fix to ensure User is requested to Sign Up/Sign In while viewing Ticket Details

--- client/pages/ticket/[ticketId].js
+++ client/pages/ticket/[ticketId].js
@@ -2,25 +2,26 @@ import Router from 'next/router';
 import useRequest from "../../hooks/use-request";
 
 const showTicket = ({ currentUser, ticket }) => {
-    // doRequest function invokes the useRequest() method which in turn calls the backend route
-    const { doRequest, errors } = useRequest({
-        url: '/api/orders',
-        method: 'post',
-        body: { ticketId: ticket.id },
-        onSuccess: (order) => Router.push('/order/[orderId]', `/order/${order.id}`)
-    });
-    
-    return (
-        <div>
-          <h1>Name: {ticket.title}</h1>
-          <h2>Price: {ticket.price}</h2>
-          {errors}
-          {ticket.userId !== currentUser.id
-            ? ( <button onClick={() => doRequest()} className="btn btn-primary">Purchase</button> )
-            : ( <button onClick={() => doRequest()} className="btn btn-danger">Delete</button> )
-          }
-        </div>
-    );
+  // doRequest function invokes the useRequest() method which in turn calls the backend route
+  const { doRequest, errors } = useRequest({
+      url: '/api/orders',
+      method: 'post',
+      body: { ticketId: ticket.id },
+      onSuccess: (order) => Router.push('/order/[orderId]', `/order/${order.id}`)
+  });
+  
+  return (
+      <div>
+        <h1>Name: {ticket.title}</h1>
+        <h2>Price: {ticket.price}</h2>
+        {errors}
+        {currentUser && (ticket.userId !== currentUser.id
+          ? ( <button onClick={() => doRequest()} className="btn btn-primary">Purchase</button> )
+          : ( <button onClick={() => doRequest()} className="btn btn-danger">Delete</button> )
+        )}
+        {!currentUser && ( <span>Please Sign Up/Sign In To Purchase</span> )}
+      </div>
+  );
 };
 
 showTicket.getInitialProps = async (context, client, currentUser) => {
